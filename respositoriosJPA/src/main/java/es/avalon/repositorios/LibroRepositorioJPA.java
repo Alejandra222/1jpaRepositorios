65c0fe47a35ef624f868e3f96db54215704d4590
Formacion <Formacion@DESKTOP-RKB56PK>
2019-03-26 18:44:08 +0100

    EntityManagerFactory y EntityManager: list-insert-update-delete

--- respositoriosJPA/src/main/java/es/avalon/repositorios/LibroRepositorioJPA.java
+++ respositoriosJPA/src/main/java/es/avalon/repositorios/LibroRepositorioJPA.java
@@ -2,12 +2,14 @@ package es.avalon.repositorios;
 
 import java.util.List;
 
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
 import es.avalon.jpa.negocio.Libro;
+
 
 public class LibroRepositorioJPA {
 
@@ -22,4 +24,80 @@ public class LibroRepositorioJPA {
 		
 		
 	}
+	
+	public void insertar() {
+		Libro libro = new Libro("RepositoryJPA", "Manuel", 450);
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("UnidadLibros");
+
+		EntityManager em = emf.createEntityManager();
+
+		try {
+			em.getTransaction().begin();
+			// persist: inserta
+			em.persist(libro);
+			em.getTransaction().commit();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		} finally {
+			em.close();
+
+		}
+	}
+	
+	private static void update() {
+		EntityManagerFactory emf =Persistence.createEntityManagerFactory("UnidadOrdenador");
+		 
+		 EntityManager em = emf.createEntityManager();
+		 
+		 //find: filtra
+		 Libro l2=em.find(Libro.class, "RepositoryJPA");
+		 
+		 l2.setPaginas(1000);
+		 
+		 
+		 try {
+		 em.getTransaction().begin();
+		 
+		 //merge: update
+		 em.merge(l2);
+		 
+		 em.getTransaction().commit();
+		 } catch (Exception e) {
+		 
+		 e.printStackTrace();
+		 }finally {
+		 em.close();
+		 
+		 }
+	}
+	
+	private static void delete() {
+		EntityManagerFactory emf =Persistence.createEntityManagerFactory("UnidadOrdenador");
+		 
+		 EntityManager em = emf.createEntityManager();
+		 
+		 //find: filtra
+		 Libro l2=em.find(Libro.class, "RepositoryJPA");
+		 
+		 
+		 try {
+		 em.getTransaction().begin();
+	
+		 
+		 //remove: delete
+		 em.remove(l2);
+		 
+		 em.getTransaction().commit();
+		 } catch (Exception e) {
+		 
+		 e.printStackTrace();
+		 }finally {
+		 em.close();
+		 
+		 }
+}
+
+	
 }
