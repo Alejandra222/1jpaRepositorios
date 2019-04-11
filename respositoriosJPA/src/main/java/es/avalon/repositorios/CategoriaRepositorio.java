e1667b225a016fd6f464a26d83afa062cafa2712
Formacion <Formacion@DESKTOP-RKB56PK>
2019-04-11 18:57:55 +0200

    Categorias

---
+++ respositoriosJPA/src/main/java/es/avalon/repositorios/CategoriaRepositorio.java
@@ -0,0 +1,25 @@
+package es.avalon.repositorios;
+
+
+
+import es.avalon.jpa.negocio.Categoria;
+
+
+public interface CategoriaRepositorio extends GenericRepository<Categoria, Integer> {
+
+	//List<Libro> buscarTodos();
+
+	//void insertar(Libro libro);
+
+	// libro le recibo desde la web new , DETACHED
+	//void delete(Libro l2);
+
+	//Libro buscarUno(String titulo);
+
+	//void salvar(Libro libro);
+
+	//List<Libro> searchLibro(String titulo);
+
+	//List<Libro> buscarTodosOrdenados(String campo);
+
+}
---
+++ respositoriosJPA/src/main/java/es/avalon/repositorios/jpa/CategoriaRepositorioJPA.java
@@ -0,0 +1,66 @@
+package es.avalon.repositorios.jpa;
+
+import org.springframework.stereotype.Repository;
+
+import es.avalon.jpa.negocio.Categoria;
+import es.avalon.repositorios.CategoriaRepositorio;
+import es.avalon.repositorios.jpa.generic.GenericRepositoryJPA;
+
+//@Component
+//public class LibroRepositorioJPA implements LibroRepositorio {
+@Repository
+public class CategoriaRepositorioJPA extends GenericRepositoryJPA<Categoria,Integer> implements CategoriaRepositorio {
+
+
+	
+
+	public CategoriaRepositorioJPA() {
+		super(Categoria.class);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	/*
+	 *  TODO LO COMENTADO ESTA EN --> GenericRepositoryJPA
+	
+	@PersistenceContext
+	EntityManager em;
+
+
+	public List<Libro> buscarTodos() {	
+	TypedQuery<Libro> consulta = em.createQuery("select l from Libro l", Libro.class);
+	return consulta.getResultList();
+
+	}
+
+
+	public void insertar(Libro libro) {
+		em.persist(libro);	
+	}
+
+
+	public void delete(Libro l2) {
+
+		Libro libroGestionado = em.merge(l2);
+		em.remove(libroGestionado);
+
+	}
+
+public Libro buscarUno(String titulo) {
+
+	Libro libro = em.find(Libro.class, titulo);
+	return libro;
+
+}
+
+	public void salvar(Libro libro) {
+
+		em.merge(libro);
+
+	}
+
+*/
+
+
+
+}
